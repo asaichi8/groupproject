bc0be7c89e22b5082fae140b0abcd47d9f36429e
asaichi8 <92694540+asaichi8@users.noreply.github.com>
2023-02-23 12:03:45 +0000

    test

--- ShoppingListApp/Program.cs
+++ ShoppingListApp/Program.cs
@@ -11,7 +11,7 @@ namespace ShoppingListApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frm_Login());
+            Application.Run(new frm_Login()); //test
         }
     }
 }
