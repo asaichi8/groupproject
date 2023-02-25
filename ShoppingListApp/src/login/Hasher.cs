f58ec65c6969b637e84705a3efe272d24143dc64
asaichi8 <92694540+asaichi8@users.noreply.github.com>
2023-02-25 11:04:01 +0000

    eliminated magic numbers in Hasher class

--- ShoppingListApp/src/login/Hasher.cs
+++ ShoppingListApp/src/login/Hasher.cs
@@ -6,13 +6,19 @@ namespace ShoppingListApp.src.Login
 {
     public static class Hasher
     {
+        private const int SALT_SIZE_BYTES = 32;
+        private const int HASH_SIZE_BYTES = 32;
+        
+        private const int MEMORY_TO_USE_KB = 1024 * 64; // 64mb
+        private const int NO_OF_ITERATIONS = 4; // lowest amount of iterations recommended
+
         /// <summary>
         /// Generates a random 32-byte salt for enhanced security.
         /// </summary>
         /// <returns>32-byte array containing the salt.</returns>
         private static byte[] GenerateSalt()
         {
-            byte[] salt = new byte[32];
+            byte[] salt = new byte[SALT_SIZE_BYTES];
             using (var rng = RandomNumberGenerator.Create())
             {
                 rng.GetBytes(salt);
@@ -24,23 +30,26 @@ namespace ShoppingListApp.src.Login
         /// <summary>
         /// Salts and hashes a password to ensure it isn't stored in plain text.
         /// </summary>
+        /// <param name="s">The string to be hashed.</param>
         /// <returns>32-byte array containing the salted and hashed password.</returns>
         private static byte[] HashAndSaltText(string s, byte[] salt)
         {
+            int coresAvailable = Environment.ProcessorCount;
+
             byte[] hashedPassword = new Argon2id(Encoding.UTF8.GetBytes(s))
             {
                 Salt = salt,
-                DegreeOfParallelism = 8,
-                MemorySize = 1024 * 64,
-                Iterations = 4
-            }.GetBytes(32);
+                DegreeOfParallelism = coresAvailable, // use half of available cores
+                MemorySize = MEMORY_TO_USE_KB,
+                Iterations = NO_OF_ITERATIONS
+            }.GetBytes(HASH_SIZE_BYTES);
 
             return hashedPassword;
         }
 
         /// <summary>
         /// A salt needs to be stored alongside the hashed text, and so this function combines
-        /// the salt and hashed password into a single 64-byte array.
+        /// the salt and hashed password into a single array.
         /// </summary>
         /// <param name="s">The string to be hashed.</param>
         /// <returns>64-byte array containing the salt (32 bytes) + hashed text (32 bytes).</returns>
@@ -49,7 +58,7 @@ namespace ShoppingListApp.src.Login
             byte[] salt = GenerateSalt();
             byte[] szHashedPass = HashAndSaltText(s, salt);
 
-            byte[] combined = new byte[64];
+            byte[] combined = new byte[SALT_SIZE_BYTES + HASH_SIZE_BYTES];
 
             Buffer.BlockCopy(salt, 0, combined, 0, salt.Length); //copy salt to first 32 bytes of "combined"
             Buffer.BlockCopy(szHashedPass, 0, combined, salt.Length, szHashedPass.Length); // copy hashed+salted password to last 32 bytes of "combined"
@@ -65,8 +74,8 @@ namespace ShoppingListApp.src.Login
         /// <returns>TRUE if success, FALSE if failed.</returns>
         public static bool VerifyHash(string text, byte[] saltHash)
         {
-            byte[] salt = new byte[32];
-            byte[] pass = new byte[32];
+            byte[] salt = new byte[SALT_SIZE_BYTES];
+            byte[] pass = new byte[SALT_SIZE_BYTES]; 
 
             Buffer.BlockCopy(saltHash, 0, salt, 0, salt.Length);
             Buffer.BlockCopy(saltHash, salt.Length, pass, 0, pass.Length);
