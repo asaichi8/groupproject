c186986efb4491706d510293fde32cab8e405428
asaichi8 <92694540+asaichi8@users.noreply.github.com>
2023-02-25 10:41:26 +0000

    additional xml documentation

--- ShoppingListApp/src/FormExtensions.cs
+++ ShoppingListApp/src/FormExtensions.cs
@@ -2,23 +2,33 @@
 {
     public static class FormExtensions
     {
-        // set the location of targetForm to the center of the current form
-        public static void CenterChild(this Form targetForm, Form callerForm)
+        /// <summary>
+        /// Sets the location of the child <see cref='Form'/> to the center of the parent <see cref='Form'/>.
+        /// </summary>
+        /// <param name="childForm">The <see cref='Form'/> to be centered.</param>
+        /// <param name="parentForm">The child's position will be centered based around this <see cref='Form'/>.</param>
+        public static void CenterChild(this Form childForm, Form parentForm)
         {
-            targetForm.Location = new Point(
-                callerForm.Location.X + (callerForm.Size.Width / 2) - (targetForm.Size.Width / 2),
-                callerForm.Location.Y + (callerForm.Size.Height / 2) - (targetForm.Size.Height / 2)
+            childForm.Location = new Point(
+                parentForm.Location.X + (parentForm.Size.Width / 2) - (childForm.Size.Width / 2),
+                parentForm.Location.Y + (parentForm.Size.Height / 2) - (childForm.Size.Height / 2)
             );
         }
 
-        public static void SwapForm(this Form targetForm, Form callerForm, bool shouldClose = false)
+        /// <summary>
+        /// Swaps the visibility of two forms.
+        /// </summary>
+        /// <param name="childForm">The <see cref='Form'/> to be displayed.</param>
+        /// <param name="parentForm">The <see cref='Form'/> to be hidden/closed.</param>
+        /// <param name="shouldClose">Determines whether the <see cref='Form.Close'/> or <see cref='Control.Hide()'/> method should be called.</param>
+        public static void SwapForm(this Form childForm, Form parentForm, bool shouldClose = false)
         {
-            targetForm.Show();
+            childForm.Show();
 
             if (shouldClose)
-                callerForm.Close();
+                parentForm.Close();
             else
-                callerForm.Hide();
+                parentForm.Hide();
         }
     }
 }
