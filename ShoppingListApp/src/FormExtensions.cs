a7d8408bac25bcc47cbee1e399dd329dc2a0ccb3
asaichi8 <92694540+asaichi8@users.noreply.github.com>
2023-02-25 10:23:25 +0000

    created extension class for Form along with CenterChild and SwapForm methods

---
+++ ShoppingListApp/src/FormExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace ShoppingListApp.src
+{
+    public static class FormExtensions
+    {
+        // set the location of targetForm to the center of the current form
+        public static void CenterChild(this Form targetForm, Form callerForm)
+        {
+            targetForm.Location = new Point(
+                callerForm.Location.X + (callerForm.Size.Width / 2) - (targetForm.Size.Width / 2),
+                callerForm.Location.Y + (callerForm.Size.Height / 2) - (targetForm.Size.Height / 2)
+            );
+        }
+
+        public static void SwapForm(this Form targetForm, Form callerForm, bool shouldClose = false)
+        {
+            targetForm.Show();
+
+            if (shouldClose)
+                callerForm.Close();
+            else
+                callerForm.Hide();
+        }
+    }
+}
