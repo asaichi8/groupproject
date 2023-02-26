497d9a8712a0db41a79c032129310d92adbfae6b
Samuel Lineham <samuel.lineham@hotmail.com>
2023-02-26 16:17:49 +0000

    The images on the welcome page are now stored within the project files to avoid errors.

--- ShoppingListApp/Properties/Resources.Designer.cs
+++ ShoppingListApp/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace ShoppingListApp.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BrainVire_Logo {
+            get {
+                object obj = ResourceManager.GetObject("BrainVire Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap UFix_Logo {
+            get {
+                object obj = ResourceManager.GetObject("UFix Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- ShoppingListApp/src/welcome/WelcomeForm.Designer.cs
+++ ShoppingListApp/src/welcome/WelcomeForm.Designer.cs
@@ -28,141 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHomePage));
-            lblWelcomeMessage = new Label();
-            pbxUFix = new PictureBox();
-            pbxBrainVire = new PictureBox();
-            txtSearch = new TextBox();
-            btnToList = new Button();
-            btnFilter = new Button();
-            btnSearch = new Button();
-            lblUsername = new Label();
-            btnLogout = new Button();
-            ((System.ComponentModel.ISupportInitialize)pbxUFix).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pbxBrainVire).BeginInit();
-            SuspendLayout();
+            this.lblWelcomeMessage = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnToList = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.pbxBrainVire = new System.Windows.Forms.PictureBox();
+            this.pbxUFix = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBrainVire)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUFix)).BeginInit();
+            this.SuspendLayout();
             // 
             // lblWelcomeMessage
             // 
-            lblWelcomeMessage.AutoSize = true;
-            lblWelcomeMessage.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            lblWelcomeMessage.Location = new Point(61, 7);
-            lblWelcomeMessage.Name = "lblWelcomeMessage";
-            lblWelcomeMessage.Size = new Size(534, 25);
-            lblWelcomeMessage.TabIndex = 0;
-            lblWelcomeMessage.Text = "Welcome to the Brainvire Ltd. Shopping Comparison Program.";
-            // 
-            // pbxUFix
-            // 
-            pbxUFix.Image = (Image)resources.GetObject("pbxUFix.Image");
-            pbxUFix.Location = new Point(96, 44);
-            pbxUFix.Margin = new Padding(3, 2, 3, 2);
-            pbxUFix.Name = "pbxUFix";
-            pbxUFix.Size = new Size(105, 112);
-            pbxUFix.SizeMode = PictureBoxSizeMode.Zoom;
-            pbxUFix.TabIndex = 1;
-            pbxUFix.TabStop = false;
-            // 
-            // pbxBrainVire
-            // 
-            pbxBrainVire.Image = (Image)resources.GetObject("pbxBrainVire.Image");
-            pbxBrainVire.Location = new Point(430, 69);
-            pbxBrainVire.Margin = new Padding(3, 2, 3, 2);
-            pbxBrainVire.Name = "pbxBrainVire";
-            pbxBrainVire.Size = new Size(224, 87);
-            pbxBrainVire.SizeMode = PictureBoxSizeMode.Zoom;
-            pbxBrainVire.TabIndex = 2;
-            pbxBrainVire.TabStop = false;
+            this.lblWelcomeMessage.AutoSize = true;
+            this.lblWelcomeMessage.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.lblWelcomeMessage.Location = new System.Drawing.Point(52, 6);
+            this.lblWelcomeMessage.Name = "lblWelcomeMessage";
+            this.lblWelcomeMessage.Size = new System.Drawing.Size(534, 25);
+            this.lblWelcomeMessage.TabIndex = 0;
+            this.lblWelcomeMessage.Text = "Welcome to the Brainvire Ltd. Shopping Comparison Program.";
             // 
             // txtSearch
             // 
-            txtSearch.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            txtSearch.Location = new Point(162, 172);
-            txtSearch.Margin = new Padding(3, 2, 3, 2);
-            txtSearch.Name = "txtSearch";
-            txtSearch.Size = new Size(318, 32);
-            txtSearch.TabIndex = 1;
-            txtSearch.TextChanged += txtSearch_TextChanged;
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.txtSearch.Location = new System.Drawing.Point(139, 149);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(273, 32);
+            this.txtSearch.TabIndex = 1;
             // 
             // btnToList
             // 
-            btnToList.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            btnToList.Location = new Point(42, 225);
-            btnToList.Margin = new Padding(3, 2, 3, 2);
-            btnToList.Name = "btnToList";
-            btnToList.Size = new Size(200, 67);
-            btnToList.TabIndex = 3;
-            btnToList.Text = "Go to shopping list";
-            btnToList.UseVisualStyleBackColor = true;
-            btnToList.Click += btnToList_Click;
+            this.btnToList.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.btnToList.Location = new System.Drawing.Point(36, 195);
+            this.btnToList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnToList.Name = "btnToList";
+            this.btnToList.Size = new System.Drawing.Size(171, 58);
+            this.btnToList.TabIndex = 3;
+            this.btnToList.Text = "Go to shopping list";
+            this.btnToList.UseVisualStyleBackColor = true;
             // 
             // btnFilter
             // 
-            btnFilter.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            btnFilter.Location = new Point(455, 225);
-            btnFilter.Margin = new Padding(3, 2, 3, 2);
-            btnFilter.Name = "btnFilter";
-            btnFilter.Size = new Size(200, 67);
-            btnFilter.TabIndex = 4;
-            btnFilter.Text = "Filter Shops";
-            btnFilter.UseVisualStyleBackColor = true;
-            btnFilter.Click += btnFilter_Click;
+            this.btnFilter.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.btnFilter.Location = new System.Drawing.Point(390, 195);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(171, 58);
+            this.btnFilter.TabIndex = 4;
+            this.btnFilter.Text = "Filter Shops";
+            this.btnFilter.UseVisualStyleBackColor = true;
             // 
             // btnSearch
             // 
-            btnSearch.Location = new Point(485, 176);
-            btnSearch.Margin = new Padding(3, 2, 3, 2);
-            btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(123, 26);
-            btnSearch.TabIndex = 2;
-            btnSearch.Text = "Search";
-            btnSearch.UseVisualStyleBackColor = true;
-            btnSearch.Click += btnSearch_Click;
+            this.btnSearch.Location = new System.Drawing.Point(416, 153);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(105, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // lblUsername
             // 
-            lblUsername.AutoSize = true;
-            lblUsername.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            lblUsername.Location = new Point(12, 309);
-            lblUsername.Name = "lblUsername";
-            lblUsername.Size = new Size(41, 20);
-            lblUsername.TabIndex = 7;
-            lblUsername.Text = "User:";
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.lblUsername.Location = new System.Drawing.Point(10, 268);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(41, 20);
+            this.lblUsername.TabIndex = 7;
+            this.lblUsername.Text = "User:";
             // 
             // btnLogout
             // 
-            btnLogout.Location = new Point(582, 297);
-            btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(106, 35);
-            btnLogout.TabIndex = 5;
-            btnLogout.Text = "Log out";
-            btnLogout.UseVisualStyleBackColor = true;
-            btnLogout.Click += btnLogout_Click;
+            this.btnLogout.Location = new System.Drawing.Point(499, 257);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(91, 30);
+            this.btnLogout.TabIndex = 5;
+            this.btnLogout.Text = "Log out";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // pbxBrainVire
+            // 
+            this.pbxBrainVire.Image = global::ShoppingListApp.Properties.Resources.BrainVire_Logo;
+            this.pbxBrainVire.Location = new System.Drawing.Point(369, 60);
+            this.pbxBrainVire.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbxBrainVire.Name = "pbxBrainVire";
+            this.pbxBrainVire.Size = new System.Drawing.Size(192, 75);
+            this.pbxBrainVire.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxBrainVire.TabIndex = 2;
+            this.pbxBrainVire.TabStop = false;
+            // 
+            // pbxUFix
+            // 
+            this.pbxUFix.Image = global::ShoppingListApp.Properties.Resources.UFix_Logo;
+            this.pbxUFix.Location = new System.Drawing.Point(82, 38);
+            this.pbxUFix.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbxUFix.Name = "pbxUFix";
+            this.pbxUFix.Size = new System.Drawing.Size(90, 97);
+            this.pbxUFix.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxUFix.TabIndex = 1;
+            this.pbxUFix.TabStop = false;
             // 
             // FormHomePage
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.Info;
-            ClientSize = new Size(700, 338);
-            Controls.Add(btnLogout);
-            Controls.Add(lblUsername);
-            Controls.Add(btnSearch);
-            Controls.Add(btnFilter);
-            Controls.Add(btnToList);
-            Controls.Add(txtSearch);
-            Controls.Add(pbxBrainVire);
-            Controls.Add(pbxUFix);
-            Controls.Add(lblWelcomeMessage);
-            Margin = new Padding(3, 2, 3, 2);
-            Name = "FormHomePage";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Welcome";
-            Load += frmHomePage_Load;
-            ((System.ComponentModel.ISupportInitialize)pbxUFix).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pbxBrainVire).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(600, 293);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.btnToList);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.pbxBrainVire);
+            this.Controls.Add(this.pbxUFix);
+            this.Controls.Add(this.lblWelcomeMessage);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormHomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBrainVire)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUFix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
