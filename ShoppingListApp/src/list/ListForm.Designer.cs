93d1ffe56a93c4bc37fd35477999768d019e3ba0
asaichi8 <92694540+asaichi8@users.noreply.github.com>
2023-02-27 17:28:33 +0000

    made list form a bit smaller

--- ShoppingListApp/src/list/ListForm.Designer.cs
+++ ShoppingListApp/src/list/ListForm.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnReturn = new System.Windows.Forms.Button();
             this.lblListForUser = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -45,10 +45,10 @@
             this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReturn.Font = new System.Drawing.Font("Nirmala UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReturn.ForeColor = System.Drawing.Color.White;
-            this.btnReturn.Location = new System.Drawing.Point(14, 13);
+            this.btnReturn.Location = new System.Drawing.Point(-4, 577);
             this.btnReturn.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(379, 88);
+            this.btnReturn.Size = new System.Drawing.Size(570, 88);
             this.btnReturn.TabIndex = 0;
             this.btnReturn.Text = "< Return to welcome page";
             this.btnReturn.UseVisualStyleBackColor = true;
@@ -59,7 +59,7 @@
             this.lblListForUser.AutoSize = true;
             this.lblListForUser.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblListForUser.ForeColor = System.Drawing.Color.White;
-            this.lblListForUser.Location = new System.Drawing.Point(9, 923);
+            this.lblListForUser.Location = new System.Drawing.Point(14, 9);
             this.lblListForUser.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblListForUser.Name = "lblListForUser";
             this.lblListForUser.Size = new System.Drawing.Size(296, 19);
@@ -69,20 +69,20 @@
             // chart1
             // 
             this.chart1.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Raised;
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(678, 0);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(564, 0);
             this.chart1.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(628, 970);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(501, 665);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chrt_Shops";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
@@ -93,7 +93,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Goldenrod;
             this.CancelButton = this.btnReturn;
-            this.ClientSize = new System.Drawing.Size(1306, 970);
+            this.ClientSize = new System.Drawing.Size(1065, 665);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.lblListForUser);
             this.Controls.Add(this.btnReturn);
