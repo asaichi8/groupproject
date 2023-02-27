b29a6af5005d63d9eb85639ab68600171c9134aa
asaichi8 <92694540+asaichi8@users.noreply.github.com>
2023-02-27 08:48:44 +0000

    fixed naming, fixed missing filter button

--- ShoppingListApp/src/search/SearchForm.Designer.cs
+++ ShoppingListApp/src/search/SearchForm.Designer.cs
@@ -28,34 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
             this.txtSearch = new System.Windows.Forms.TextBox();
-            this.wbv_Asda = new Microsoft.Web.WebView2.WinForms.WebView2();
-            this.wbv_Tesco = new Microsoft.Web.WebView2.WinForms.WebView2();
-            this.wbv_Sainsburys = new Microsoft.Web.WebView2.WinForms.WebView2();
-            this.btn_Search = new System.Windows.Forms.Button();
-            this.btn_Filter = new System.Windows.Forms.Button();
-            this.lbl_Asda = new System.Windows.Forms.Label();
-            this.lbl_Tesco = new System.Windows.Forms.Label();
-            this.lbl_Sainsburys = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.wbv_Asda)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.wbv_Tesco)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.wbv_Sainsburys)).BeginInit();
+            this.wbvAsda = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.wbvTesco = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.wbvSainsburys = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.lblAsda = new System.Windows.Forms.Label();
+            this.lblTesco = new System.Windows.Forms.Label();
+            this.lblSainsburys = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wbvAsda)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wbvTesco)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wbvSainsburys)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnSearch
+            // btnBack
             // 
-            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSearch.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Location = new System.Drawing.Point(20, 20);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(248, 105);
-            this.btnSearch.TabIndex = 0;
-            this.btnSearch.Text = "Return to search menu";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(20, 20);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(248, 105);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Return to search menu";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // txtSearch
             // 
@@ -65,138 +65,138 @@
             this.txtSearch.Location = new System.Drawing.Point(280, 39);
             this.txtSearch.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(897, 37);
+            this.txtSearch.Size = new System.Drawing.Size(897, 25);
             this.txtSearch.TabIndex = 4;
             // 
-            // wbv_Asda
+            // wbvAsda
             // 
-            this.wbv_Asda.AllowExternalDrop = true;
-            this.wbv_Asda.CreationProperties = null;
-            this.wbv_Asda.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.wbv_Asda.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(232)))));
-            this.wbv_Asda.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.wbv_Asda.Location = new System.Drawing.Point(12, 200);
-            this.wbv_Asda.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
-            this.wbv_Asda.Name = "wbv_Asda";
-            this.wbv_Asda.Size = new System.Drawing.Size(608, 892);
-            this.wbv_Asda.Source = new System.Uri("https://groceries.asda.com", System.UriKind.Absolute);
-            this.wbv_Asda.TabIndex = 5;
-            this.wbv_Asda.ZoomFactor = 1D;
+            this.wbvAsda.AllowExternalDrop = true;
+            this.wbvAsda.CreationProperties = null;
+            this.wbvAsda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wbvAsda.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(232)))));
+            this.wbvAsda.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.wbvAsda.Location = new System.Drawing.Point(12, 200);
+            this.wbvAsda.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.wbvAsda.Name = "wbvAsda";
+            this.wbvAsda.Size = new System.Drawing.Size(608, 892);
+            this.wbvAsda.Source = new System.Uri("https://groceries.asda.com", System.UriKind.Absolute);
+            this.wbvAsda.TabIndex = 5;
+            this.wbvAsda.ZoomFactor = 1D;
             // 
-            // wbv_Tesco
+            // wbvTesco
             // 
-            this.wbv_Tesco.AllowExternalDrop = true;
-            this.wbv_Tesco.CreationProperties = null;
-            this.wbv_Tesco.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.wbv_Tesco.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(232)))));
-            this.wbv_Tesco.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.wbv_Tesco.Location = new System.Drawing.Point(676, 200);
-            this.wbv_Tesco.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
-            this.wbv_Tesco.Name = "wbv_Tesco";
-            this.wbv_Tesco.Size = new System.Drawing.Size(608, 892);
-            this.wbv_Tesco.Source = new System.Uri("https://www.tesco.com/groceries/en-GB", System.UriKind.Absolute);
-            this.wbv_Tesco.TabIndex = 6;
-            this.wbv_Tesco.ZoomFactor = 1D;
+            this.wbvTesco.AllowExternalDrop = true;
+            this.wbvTesco.CreationProperties = null;
+            this.wbvTesco.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wbvTesco.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(232)))));
+            this.wbvTesco.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.wbvTesco.Location = new System.Drawing.Point(676, 200);
+            this.wbvTesco.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.wbvTesco.Name = "wbvTesco";
+            this.wbvTesco.Size = new System.Drawing.Size(608, 892);
+            this.wbvTesco.Source = new System.Uri("https://www.tesco.com/groceries/en-GB", System.UriKind.Absolute);
+            this.wbvTesco.TabIndex = 6;
+            this.wbvTesco.ZoomFactor = 1D;
             // 
-            // wbv_Sainsburys
+            // wbvSainsburys
             // 
-            this.wbv_Sainsburys.AllowExternalDrop = true;
-            this.wbv_Sainsburys.CreationProperties = null;
-            this.wbv_Sainsburys.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.wbv_Sainsburys.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(232)))));
-            this.wbv_Sainsburys.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.wbv_Sainsburys.Location = new System.Drawing.Point(1336, 200);
-            this.wbv_Sainsburys.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
-            this.wbv_Sainsburys.Name = "wbv_Sainsburys";
-            this.wbv_Sainsburys.Size = new System.Drawing.Size(608, 892);
-            this.wbv_Sainsburys.Source = new System.Uri("https://www.sainsburys.co.uk", System.UriKind.Absolute);
-            this.wbv_Sainsburys.TabIndex = 7;
-            this.wbv_Sainsburys.ZoomFactor = 1D;
+            this.wbvSainsburys.AllowExternalDrop = true;
+            this.wbvSainsburys.CreationProperties = null;
+            this.wbvSainsburys.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wbvSainsburys.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(232)))));
+            this.wbvSainsburys.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.wbvSainsburys.Location = new System.Drawing.Point(1336, 200);
+            this.wbvSainsburys.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.wbvSainsburys.Name = "wbvSainsburys";
+            this.wbvSainsburys.Size = new System.Drawing.Size(608, 892);
+            this.wbvSainsburys.Source = new System.Uri("https://www.sainsburys.co.uk", System.UriKind.Absolute);
+            this.wbvSainsburys.TabIndex = 7;
+            this.wbvSainsburys.ZoomFactor = 1D;
             // 
-            // btn_Search
+            // btnSearch
             // 
-            this.btn_Search.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_Search.Location = new System.Drawing.Point(1227, 39);
-            this.btn_Search.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
-            this.btn_Search.Name = "btn_Search";
-            this.btn_Search.Size = new System.Drawing.Size(205, 69);
-            this.btn_Search.TabIndex = 8;
-            this.btn_Search.Text = "Search";
-            this.btn_Search.UseVisualStyleBackColor = true;
-            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.Location = new System.Drawing.Point(1227, 39);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(205, 69);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // btn_Filter
+            // btnFilter
             // 
-            this.btn_Filter.BackColor = System.Drawing.Color.Goldenrod;
-            this.btn_Filter.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_Filter.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
-            this.btn_Filter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Filter.Font = new System.Drawing.Font("Nirmala UI", 14F, System.Drawing.FontStyle.Bold);
-            this.btn_Filter.ForeColor = System.Drawing.Color.White;
-            this.btn_Filter.Location = new System.Drawing.Point(888, 1137);
-            this.btn_Filter.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
-            this.btn_Filter.Name = "btn_Filter";
-            this.btn_Filter.Size = new System.Drawing.Size(244, 120);
-            this.btn_Filter.TabIndex = 9;
-            this.btn_Filter.Text = "Filter Shops";
-            this.btn_Filter.UseVisualStyleBackColor = false;
-            this.btn_Filter.Click += new System.EventHandler(this.btn_Filter_Click);
+            this.btnFilter.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnFilter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFilter.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
+            this.btnFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFilter.Font = new System.Drawing.Font("Nirmala UI", 14F, System.Drawing.FontStyle.Bold);
+            this.btnFilter.ForeColor = System.Drawing.Color.White;
+            this.btnFilter.Location = new System.Drawing.Point(1727, 11);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(217, 53);
+            this.btnFilter.TabIndex = 9;
+            this.btnFilter.Text = "Filter Shops";
+            this.btnFilter.UseVisualStyleBackColor = false;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
             // 
-            // lbl_Asda
+            // lblAsda
             // 
-            this.lbl_Asda.AutoSize = true;
-            this.lbl_Asda.Location = new System.Drawing.Point(275, 165);
-            this.lbl_Asda.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lbl_Asda.Name = "lbl_Asda";
-            this.lbl_Asda.Size = new System.Drawing.Size(58, 28);
-            this.lbl_Asda.TabIndex = 10;
-            this.lbl_Asda.Text = "Asda";
+            this.lblAsda.AutoSize = true;
+            this.lblAsda.Location = new System.Drawing.Point(275, 165);
+            this.lblAsda.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblAsda.Name = "lblAsda";
+            this.lblAsda.Size = new System.Drawing.Size(42, 19);
+            this.lblAsda.TabIndex = 10;
+            this.lblAsda.Text = "Asda";
             // 
-            // lbl_Tesco
+            // lblTesco
             // 
-            this.lbl_Tesco.AutoSize = true;
-            this.lbl_Tesco.Location = new System.Drawing.Point(947, 165);
-            this.lbl_Tesco.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lbl_Tesco.Name = "lbl_Tesco";
-            this.lbl_Tesco.Size = new System.Drawing.Size(64, 28);
-            this.lbl_Tesco.TabIndex = 11;
-            this.lbl_Tesco.Text = "Tesco";
+            this.lblTesco.AutoSize = true;
+            this.lblTesco.Location = new System.Drawing.Point(947, 165);
+            this.lblTesco.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblTesco.Name = "lblTesco";
+            this.lblTesco.Size = new System.Drawing.Size(46, 19);
+            this.lblTesco.TabIndex = 11;
+            this.lblTesco.Text = "Tesco";
             // 
-            // lbl_Sainsburys
+            // lblSainsburys
             // 
-            this.lbl_Sainsburys.AutoSize = true;
-            this.lbl_Sainsburys.Location = new System.Drawing.Point(1588, 165);
-            this.lbl_Sainsburys.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lbl_Sainsburys.Name = "lbl_Sainsburys";
-            this.lbl_Sainsburys.Size = new System.Drawing.Size(114, 28);
-            this.lbl_Sainsburys.TabIndex = 12;
-            this.lbl_Sainsburys.Text = "Sainsburys";
+            this.lblSainsburys.AutoSize = true;
+            this.lblSainsburys.Location = new System.Drawing.Point(1588, 165);
+            this.lblSainsburys.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSainsburys.Name = "lblSainsburys";
+            this.lblSainsburys.Size = new System.Drawing.Size(81, 19);
+            this.lblSainsburys.TabIndex = 12;
+            this.lblSainsburys.Text = "Sainsburys";
             // 
             // FormSearch
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1968, 1273);
-            this.Controls.Add(this.lbl_Sainsburys);
-            this.Controls.Add(this.lbl_Tesco);
-            this.Controls.Add(this.lbl_Asda);
-            this.Controls.Add(this.btn_Filter);
-            this.Controls.Add(this.btn_Search);
-            this.Controls.Add(this.wbv_Sainsburys);
-            this.Controls.Add(this.wbv_Tesco);
-            this.Controls.Add(this.wbv_Asda);
-            this.Controls.Add(this.txtSearch);
+            this.ClientSize = new System.Drawing.Size(1968, 1062);
+            this.Controls.Add(this.lblSainsburys);
+            this.Controls.Add(this.lblTesco);
+            this.Controls.Add(this.lblAsda);
+            this.Controls.Add(this.btnFilter);
             this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.wbvSainsburys);
+            this.Controls.Add(this.wbvTesco);
+            this.Controls.Add(this.wbvAsda);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnBack);
             this.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Goldenrod;
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "FormSearch";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SearchForm";
-            ((System.ComponentModel.ISupportInitialize)(this.wbv_Asda)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.wbv_Tesco)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.wbv_Sainsburys)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wbvAsda)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wbvTesco)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wbvSainsburys)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,15 +204,15 @@
 
         #endregion
 
-        private Button btnSearch;
+        private Button btnBack;
         private TextBox txtSearch;
-        private Microsoft.Web.WebView2.WinForms.WebView2 wbv_Asda;
-        private Microsoft.Web.WebView2.WinForms.WebView2 wbv_Tesco;
-        private Microsoft.Web.WebView2.WinForms.WebView2 wbv_Sainsburys;
-        private Button btn_Search;
-        private Button btn_Filter;
-        private Label lbl_Asda;
-        private Label lbl_Tesco;
-        private Label lbl_Sainsburys;
+        private Microsoft.Web.WebView2.WinForms.WebView2 wbvAsda;
+        private Microsoft.Web.WebView2.WinForms.WebView2 wbvTesco;
+        private Microsoft.Web.WebView2.WinForms.WebView2 wbvSainsburys;
+        private Button btnSearch;
+        private Button btnFilter;
+        private Label lblAsda;
+        private Label lblTesco;
+        private Label lblSainsburys;
     }
 }
--- ShoppingListApp/src/search/SearchForm.cs
+++ ShoppingListApp/src/search/SearchForm.cs
@@ -22,25 +22,24 @@ namespace ShoppingListApp
             InitializeComponent();
             txtSearch.Text = _searchItem;
             prevForm = _prevForm;
-            //wbv_Asda.EnsureCoreWebView2Async(default, default);
-            //wbv_Tesco.EnsureCoreWebView2Async(default, default);
-            //wbv_Sainsburys.EnsureCoreWebView2Async(default, default);
+            //wbvAsda.EnsureCoreWebView2Async(default, default);
+            //wbvTesco.EnsureCoreWebView2Async(default, default);
+            //wbvSainsburys.EnsureCoreWebView2Async(default, default);
         }
 
-        private void btnSearch_Click(object sender, EventArgs e)
+        private void btnBack_Click(object sender, EventArgs e)
         {
             prevForm.SwapForm(this, true);
         }
 
-        private void btn_Search_Click(object sender, EventArgs e)
+        private void btnSearch_Click(object sender, EventArgs e)
         {
-            //wbv_Asda.CoreWebView2.Navigate("https://groceries.asda.com/search/" + txtSearch.Text + "/products?sort=price+asc");
-            //wbv_Tesco.CoreWebView2.Navigate("https://www.tesco.com/groceries/en-GB/search?query=" + txtSearch.Text + "&sortBy=price-ascending");
-            //wbv_Sainsburys.CoreWebView2.Navigate("https://www.sainsburys.co.uk/gol-ui/SearchResults/"+ txtSearch.Text + "/category:/sort:price");
+            //wbvAsda.CoreWebView2.Navigate("https://groceries.asda.com/search/" + txtSearch.Text + "/products?sort=price+asc");
+            //wbvTesco.CoreWebView2.Navigate("https://www.tesco.com/groceries/en-GB/search?query=" + txtSearch.Text + "&sortBy=price-ascending");
+            //wbvSainsburys.CoreWebView2.Navigate("https://www.sainsburys.co.uk/gol-ui/SearchResults/"+ txtSearch.Text + "/category:/sort:price");
         }
 
-        // this button doesn't exist?
-        private void btn_Filter_Click(object sender, EventArgs e)
+        private void btnFilter_Click(object sender, EventArgs e)
         {
             Form filterForm = new FormFilter();
             filterForm.ShowDialog();
